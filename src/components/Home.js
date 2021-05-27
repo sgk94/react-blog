4d2fb134ccbf3970cde4a836b467b76f05258a83
Shawn <kimsha004@gmail.com>
2021-05-27 16:54:40 -0700

    more practice using state and hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
-
-  const handleClick = (e) => {
-    console.log("yo yo yo", e);
-  }
-
-  const handleClickAgain = (name, e) => {
-    console.log('hello ' + name, e.target);
-
-  }
-
+  const [blogs, setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
   return ( 
     <div className="home">
-      <h2>Homepage</h2>
-      <button onClick={handleClick}>Click Me</button>
-      <button onClick={(e) => handleClickAgain('mario', e)}>click me again</button>
+     {blogs.map((blog) => (
+       <div className="blog-preview" key={blog.id}>
+        <h2>{blog.title}</h2>
+        <div>Written by {blog.author}</div>
+       </div>
+     ))}
     </div>
    );
 }
  
 export default Home;
-<div className="home"></div>
