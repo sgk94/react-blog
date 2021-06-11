cc4880d6acbe5d2f584e904e58255e41456faf7c
Shawn <kimsha004@gmail.com>
2021-06-11 01:50:42 -0700

    add blog id

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import Navbar from './components/Navbar';
 import Home from './components/Home';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Create from './components/Create';
+import BlogDetails from './components/BlogDetails';
 
 const App = () => {
   return (
     <Router>
       <div className="App">
         <Navbar />
-      <div className="content">
-          <Switch>
-            <Route exact path="/"> 
-              <Home />
-            </Route>
-            <Route path="/create"> 
-              <Create />
-            </Route>
-          </Switch>
-      </div>
+        <div className="content">
+            <Switch>
+              <Route exact path="/"> 
+                <Home />
+              </Route>
+              <Route path="/create"> 
+                <Create />
+              </Route>
+              <Route path="/blogs/:id"> 
+                <BlogDetails />
+              </Route>
+            </Switch>
+        </div>
       </div>
     </Router>
   );
